2da8f20373a2bc45f329e4ed06b855862ef13d97
Lohane <lohanedafnevieira@gmail.com>
2022-04-18 04:00:48 -0300

    Rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
-import NavBar from "./components/NavBar";
+import {BrowserRouter, Routes,Route} from "react-router-dom"; 
+import Listing from 'Pages/Listagem';
+import Form from 'Pages/Formulario';
+import NavBar from "components/NavBar";
 
-function App() {
+function App () {
   return (
+    <BrowserRouter>
     <NavBar/>
-  );
+    <Routes>
+    <Route path="/" element={<Listing/>} />
+    <Route path="/Form">
+      <Route path=":movieId" element={<Form/>} />
+    </Route>
+    </Routes>
+    
+    </BrowserRouter>
+  )
 }
 
 export default App;
