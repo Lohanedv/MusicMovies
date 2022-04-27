aa54abbaab46c80b972970755de735c4e40891ee
Lohane Dafne Borges Vieira <lohanedafnevieira@gmail.com>
2022-04-26 23:10:06 -0300

    Delete backend directory

--- backend/src/main/java/com/Lohanedv/MusicMovie/repositories/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Lohanedv.MusicMovie.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Lohanedv.MusicMovie.entities.Movie;
-
-	public interface MovieRepository extends JpaRepository<Movie, Long>{
-		
-	}
