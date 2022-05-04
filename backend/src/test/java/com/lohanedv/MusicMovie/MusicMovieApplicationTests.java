06968dad980528e9fd4d8f8b46ccdbb84471c923
Lohane <lohanedafnevieira@gmail.com>
2022-05-04 14:10:50 -0300

    backend

---
+++ backend/src/test/java/com/lohanedv/MusicMovie/MusicMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lohanedv.MusicMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
